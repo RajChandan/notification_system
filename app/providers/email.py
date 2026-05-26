69d74f686579c444fe381d15c28227276375d034
RajChandan <raj.chandan91@gmail.com>
2026-05-27 01:05:37 +0530

    email

---
+++ app/providers/email.py
@@ -0,0 +1,9 @@
+from app.providers.base import DeliveryProvider
+
+
+class SendGridEmailProvider(DeliveryProvider):
+    async def send(
+        self, recipient: str, content: str, metadata: dict | None = None
+    ) -> bool:
+        print(f"sending mail to {recipient} : {content}")
+        return True
