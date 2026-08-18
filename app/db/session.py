e22e843439dd2fd155d30ef6242bde24278beec3
RajChandan <raj.chandan91@gmail.com>
2026-08-18 22:59:25 +0530

    postgres setup db

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
-# DATABASE_URL = "postgresql+asyncpg://postgres:password@localhost:5432/notification_db"
-DATABASE_URL = "sqlite+aiosqlite:///./notification.db"
+DATABASE_URL = "postgresql+asyncpg://notification_user:notification_password@localhost:5433/notification_db"
+# DATABASE_URL = "sqlite+aiosqlite:///./notification.db"
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(
+    DATABASE_URL, echo=False, pool_pre_ping=True, pool_size=10, max_overflow=20
+)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, expire_on_commit=False, class_=AsyncSession
