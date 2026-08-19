216c7d14d04b6b964fba55825034f68e5a9a9304
RajChandan <raj.chandan91@gmail.com>
2026-08-19 21:12:55 +0530

    alembic migrations

--- app/db/session.py
+++ app/db/session.py
@@ -10,3 +10,8 @@ engine = create_async_engine(
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, expire_on_commit=False, class_=AsyncSession
 )
+
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
